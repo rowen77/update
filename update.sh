499335ef6189df289cf289677a10d68a01d12292
rowen77 <richardowenx@gmail.com>
2020-08-14 14:37:05 +0100

    add echos and clean up commands

--- update.sh
+++ update.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+echo "-> sudo apt-get update"
 sudo apt-get update
+
+echo "-> sudo apt-get upgrade [you may be prompted to continue]"
 sudo apt-get upgrade
+
+echo "-> sudo apt-get clean"
+sudo apt-get clean
+
+echo "-> sudo apt-get --purge -y autoremove"
+sudo apt-get --purge -y autoremove
